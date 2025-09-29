a06f514d67f24ba2c6f851626b01aecd44e68666
Arslan Kutlubaev <kutlubaev99@gmail.com>
2025-09-29 11:09:56 +0500

    Добавьте файлы проекта.

---
+++ Lab_rab1_2_KutlubaevA.D._БПИ_23_02/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Lab_rab1_2_KutlubaevA.D._БПИ_23_02
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnSquareArea_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(TxtSquareSide.Text, out double side) && side > 0)
+            {
+                Square square = new Square(side);
+                double area = square.CalculateArea();
+                ResultSquare.Text = $"{square.GetInfo()}\nПлощадь: {area:F2}";
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректное значение для стороны квадрата.");
+            }
+        }
+
+        private void BtnSquarePerimeter_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(TxtSquareSide.Text, out double side) && side > 0)
+            {
+                Square square = new Square(side);
+                double perimeter = square.CalculatePerimeter();
+                ResultSquare.Text = $"{square.GetInfo()}\nПериметр: {perimeter:F2}";
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректное значение для стороны квадрата.");
+            }
+        }
+        private void BtnEllipseArea_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(TxtEllipseA.Text, out double rA) && rA > 0 &&
+                double.TryParse(TxtEllipseB.Text, out double rB) && rB > 0)
+            {
+                Ellipse ellipse = new Ellipse(rA, rB);
+                double area = ellipse.CalculateArea();
+                ResultEllipse.Text = $"{ellipse.GetInfo(true)}\nПлощадь: {area:F2}";
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные значения для полуосей эллипса.");
+            }
+        }
+
+        private void BtnEllipsePerimeter_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(TxtEllipseA.Text, out double rA) && rA > 0 &&
+                double.TryParse(TxtEllipseB.Text, out double rB) && rB > 0)
+            {
+                Ellipse ellipse = new Ellipse(rA, rB);
+                double perimeter = ellipse.CalculatePerimeter();
+                ResultEllipse.Text = $"{ellipse.GetInfo(true)}\nПериметр (приближенно): {perimeter:F2}";
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные значения для полуосей эллипса.");
+            }
+        }
+
+        private void BtnTrapArea_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(TxtTrapBaseA.Text, out double bA) && bA > 0 &&
+                double.TryParse(TxtTrapBaseB.Text, out double bB) && bB > 0 &&
+                double.TryParse(TxtTrapHeight.Text, out double h) && h > 0 &&
+                double.TryParse(TxtTrapSideC.Text, out double sC) && sC > 0 &&
+                double.TryParse(TxtTrapSideD.Text, out double sD) && sD > 0)
+            {
+                Trapezoid trapezoid = new Trapezoid(bA, bB, h, sC, sD);
+                double area = trapezoid.CalculateArea();
+                ResultTrap.Text = $"{trapezoid.GetInfo()}\nПлощадь: {area:F2}";
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные значения для всех полей трапеции.");
+            }
+        }
+
+        private void BtnTrapPerimeter_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(TxtTrapBaseA.Text, out double bA) && bA > 0 &&
+                double.TryParse(TxtTrapBaseB.Text, out double bB) && bB > 0 &&
+                double.TryParse(TxtTrapHeight.Text, out double h) && h > 0 &&
+                double.TryParse(TxtTrapSideC.Text, out double sC) && sC > 0 &&
+                double.TryParse(TxtTrapSideD.Text, out double sD) && sD > 0)
+            {
+                Trapezoid trapezoid = new Trapezoid(bA, bB, h, sC, sD);
+                double perimeter = trapezoid.CalculatePerimeter();
+                ResultTrap.Text = $"{trapezoid.GetInfo()}\nПериметр: {perimeter:F2}";
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные значения для всех полей трапеции.");
+            }
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, 0) && e.Text != ",")
+            {
+                e.Handled = true;
+            }
+
+            if (e.Text == "," && ((System.Windows.Controls.TextBox)sender).Text.Contains(","))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
